fc42a6d9110abb99879c852eb9d613305e3c767e
sibgatullah-s <43392019+sibgatullah-s@users.noreply.github.com>
2020-02-18 15:31:34 -0500

    modified tooltip onhover

--- src/PortfolioPlanning/Common/Components/ProgressDetails.tsx
+++ src/PortfolioPlanning/Common/Components/ProgressDetails.tsx
@@ -5,6 +5,8 @@ import { TooltipHost } from "office-ui-fabric-react/lib/Tooltip";
 export interface IProgressIndicator {
     total: number;
     completed: number;
+    rwork: number;
+    cwork: number;
 }
 
 export interface IProgressIndicatorProps extends IProgressIndicator {
@@ -13,18 +15,37 @@ export interface IProgressIndicatorProps extends IProgressIndicator {
 
 export class ProgressDetails extends React.Component<IProgressIndicatorProps, {}> {
     public render() {
-        const { total, completed, onClick } = this.props;
+        const { total, completed, rwork, cwork, onClick } = this.props;
 
-        if (total <= 0) {
-            return null;
-        }
+        // if (total <= 0) {
+        //     return null;
+        // }
+
+        console.log('progwork', rwork, cwork);
 
         const style = {};
         style["width"] = `${(completed * 100) / total}%`;
         const progressText = `${completed}/${total}`;
+
+        const style1 = {};
+        style1["width"] = `${(rwork * 100) / rwork}%`;
+        const progressText1 = `${rwork}`;
+
+        const style2 = {};
+        style2["width"] = `${(cwork * 100) / cwork}%`;
+        const progressText2 = `${cwork}`;
+
         return (
-            <TooltipHost content={progressText} className="progress-indicator-tooltip">
+            <TooltipHost content={"Remaining Hours: " + rwork + " Completed Hours: " + cwork + " User Stories: " + completed + "/" + total} className="progress-indicator-tooltip">
                 <div className="progress-indicator-container" onClick={onClick}>
+                    <div className="progressrwork-details-parts">
+                        <div className="progressrwork-completed" style={style1} />
+                    </div>
+                    <div className="progress-text"> {progressText1}</div>
+                    <div className="progresscwork-details-parts">
+                        <div className="progresscwork-completed" style={style2} />
+                    </div>
+                    <div className="progress-text"> {progressText2}</div>
                     <div className="progress-details-parts">
                         <div className="progress-completed" style={style} />
                     </div>
